dc28710207262b8a81397e4ceb6883ad9bdd4810
saorregog <saorregog@gmail.com>
2023-06-02 11:36:16 -0500

    Codigo del algoritmo de verificacion de numero par o impar

---
+++ 1. Numero Par o Impar/ParImpar.py
@@ -0,0 +1,8 @@
+# VERIFICAR SI EL NÚMERO INGRESADO ES PAR O IMPAR
+
+numero = int(input("Ingresa un número: "))
+
+if (numero % 2 == 0):
+    print("El número ingresado es par")
+else:
+    print("El número ingresado es impar")
