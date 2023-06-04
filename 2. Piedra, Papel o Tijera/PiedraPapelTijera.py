63853feabd4df9e4803c44e0f15d65895b88ccf6
saorregog <saorregog@gmail.com>
2023-06-04 00:10:06 -0500

    Finaliza el desarrollo del juego

--- 2. Piedra, Papel o Tijera/PiedraPapelTijera.py
+++ 2. Piedra, Papel o Tijera/PiedraPapelTijera.py
@@ -4,15 +4,40 @@ import random
 
 options = ("Piedra", "Papel", "Tijera")
 
-user_option = input("Elija Piedra, Papel o Tijera: ").capitalize()
-pc_option = random.choice(options)
+user_score = int(0)
+pc_score = int(0)
+round = int(1)
 
-print(f"\nEl usuario eligió {user_option}")
-print(f"La máquina eligió {pc_option}\n")
+while (user_score < 3 and pc_score < 3):
+    print("\n" + "*" * 10)
+    print(f" RONDA {round}")
+    print("*" * 10)
 
-if (user_option == pc_option):
-    print("¡¡EMPATE!!")
-elif ((user_option == "Piedra" and pc_option == "Tijera") or (user_option == "Papel" and pc_option == "Piedra") or (user_option == "Tijera" and pc_option == "Papel")):
-    print("¡¡EL USUARIO GANA!!")
+    user_choice = input("\nElija Piedra, Papel o Tijera: ").capitalize()
+    pc_choice = random.choice(options)
+
+    print(f"\nEl usuario eligió {user_choice}")
+    print(f"La máquina eligió {pc_choice}")
+
+    if (user_choice == pc_choice):
+        print("\n¡¡EMPATE!!")
+    elif ((user_choice == "Piedra" and pc_choice == "Tijera") or (user_choice == "Papel" and pc_choice == "Piedra") or (user_choice == "Tijera" and pc_choice == "Papel")):
+        user_score += 1
+        print("\n¡¡EL USUARIO GANA!!")
+    else:
+        pc_score += 1
+        print("\n¡¡EL USUARIO PIERDE!!")
+
+    round += 1
+
+    if (user_score == 3 or pc_score == 3):
+        continue
+    else:
+        print(f"\nEl marcador es: Usuario {user_score} - Máquina {pc_score}")
+
+print(f"\nEl marcador FINAL es: Usuario {user_score} - Máquina {pc_score}")
+
+if (user_score == 3):
+    print("\n=> EL USUARIO ES EL GANADOR DEL JUEGO <=\n")
 else:
-    print("¡¡EL USUARIO PIERDE!!")
+    print("\n=> LA MÁQUINA ES EL GANADOR DEL JUEGO <=\n")
