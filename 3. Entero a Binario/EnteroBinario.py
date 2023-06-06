bb5dc5a2e356eb07fb28e8810a0c33282cd4d571
saorregog <saorregog@gmail.com>
2023-06-06 09:47:46 -0500

    Version funcional del algoritmo de conversion de numero entero en numero binario

---
+++ 3. Entero a Binario/EnteroBinario.py
@@ -0,0 +1,12 @@
+# CONVERTIR UN NÚMERO ENTERO EN NÚMERO BINARIO
+
+def integer_to_binary(n):
+    if (n // 2 > 1):
+        b = integer_to_binary(int(n / 2)) + str(n % 2)
+    else:
+        b = str(n // 2) + str(n % 2)
+
+    return b
+
+
+print(integer_to_binary(41))
