15c855a44fe45dd49ce075f2401549892e1dd6cf
saorregog <saorregog@gmail.com>
2023-06-07 15:31:00 -0500

    Version 3 solucion ejercicio CodeWars 4 kyu

--- 5. Most Frequently Used Words/UsedWords.py
+++ 5. Most Frequently Used Words/UsedWords.py
@@ -68,3 +68,34 @@ print(top_3_words("""In a village of La Mancha, the name of which I have no desi
         coursing. An olla of rather more beef than mutton, a salad on most
         nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
         on Sundays, made away with three-quarters of his income."""))
+
+
+# VERSIÓN 3
+def top_3_words(text):
+    t_lowered = text.lower()
+
+    for i in range(len(t_lowered)):
+        if (i > 0 and i < (len(t_lowered) - 1) and t_lowered[i] == "'" and (t_lowered[i-1].isalpha() or t_lowered[i+1].isalpha())):
+            t_lowered = t_lowered.replace(t_lowered[i], "4", 1)
+        elif (not t_lowered[i].isalpha() and not t_lowered[i] == " "):
+            t_lowered = t_lowered.replace(t_lowered[i], " ", 1)
+
+    t_lowered = t_lowered.replace("4", "'").split()
+
+    char_set = set(t_lowered)
+
+    char_list_count = [[char, t_lowered.count(char)] for char in char_set]
+
+    char_list_count.sort(reverse=True, key=lambda e: e[1])
+
+    result = [e[0] for e in char_list_count][:3]
+
+    return result
+
+
+print(top_3_words("""In a village of La Mancha, the name of which I have no desire to call to
+        mind, there lived not long since one of those gentlemen that keep a lance
+        in the lance-rack, an old buckler, a lean hack, and a greyhound for
+        coursing. An olla of rather more beef than mutton, a salad on most
+        nights, scraps on Saturdays, lentils on Fridays, and a pigeon or so extra
+        on Sundays, made away with three-quarters of his income."""))
