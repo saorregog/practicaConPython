a39a7a3fb6809e996c6ed07f684a3e6fa6bdc55f
saorregog <saorregog@gmail.com>
2023-06-10 14:15:40 -0500

    Version 2 de ejercicio test-prueba

--- 14. Prueba r2longestPalindrome/r2longestPalindrome.py
+++ 14. Prueba r2longestPalindrome/r2longestPalindrome.py
@@ -1,5 +1,5 @@
 # VERSIÓN 1
-def r2longestPalindrome(strParam):
+def r2longestPalindrome1(strParam):
     strParam = strParam.lower().split()
     strParam = "".join(strParam)
 
@@ -13,6 +13,24 @@ def r2longestPalindrome(strParam):
 
     return max(result)
 
+print(r2longestPalindrome1(
+    "anitalavalatinaanitalavalatinaanitalavalatinaanitalavalatina"))
 
-print(r2longestPalindrome(
+
+# VERSIÓN 2
+def r2longestPalindrome2(strParam):
+    strParam = strParam.lower().split()
+    strParam = "".join(strParam)
+
+    result = int(0)
+
+    for i in range(len(strParam)):
+        for j in range(len(strParam), i, -1):
+            str_reversed = strParam[i:j]
+            if (str_reversed[::-1] in strParam and len(str_reversed) > result):
+                result = len(str_reversed)
+
+    return result
+
+print(r2longestPalindrome2(
     "anitalavalatinaanitalavalatinaanitalavalatinaanitalavalatina"))
