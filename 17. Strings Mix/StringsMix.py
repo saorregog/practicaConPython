14a20bb48fb1a905730fe3ddbbfab8f2bfd4aa6d
saorregog <saorregog@gmail.com>
2023-06-10 22:59:07 -0500

    Solucion parcial ejercicio CodeWars 4 kyu

--- 17. Strings Mix/StringsMix.py
+++ 17. Strings Mix/StringsMix.py
@@ -1,15 +1,30 @@
+# FALTA ORDENAR LOS RESULTADOS PRIORIZANDO PRIMER TÉRMINO ( : 1 2) Y LUEGO NÚMERO DE VECES QUE CADA LETRA SE REPITE
+
 def mix(s1, s2):
-    s1 = "".join(s1.lower().split())
-    s_1 = list(s1)
-    s1_ = list(set(s1))
-    
-#     s2 = "".join(s1.lower().split())
-    s2 = sorted(list("".join(s2.lower().split())), reverse=False)
-    s2_ = list(set(s1))
-    
-    string1 = sorted([[letter, s1.count(letter)] for letter in s1_ if s1.count(letter) > 1], key=lambda e: e[1], reverse=True)
-    string2 = sorted([[letter, s2.count(letter)] for letter in s2_ if s2.count(letter) > 1], key=lambda e: e[1], reverse=True)
-    
+    s1 = "".join([char for char in s1 if char.isalpha() and char.islower()])
+    s2 = "".join([char for char in s2 if char.isalpha() and char.islower()])
+
+    s_ = set(s1)
+
+    for letter in set(s2):
+        s_.add(letter)
+
+    s_ = sorted(list(s_), reverse=False)
+
     result = list()
-    
-    return s2
+
+    for letter in s_:
+        if (s1.count(letter) > s2.count(letter) and s1.count(letter) > 1):
+            result.append(["1", letter, s1.count(letter)])
+        elif (s2.count(letter) > s1.count(letter) and s2.count(letter) > 1):
+            result.append(["2", letter, s2.count(letter)])
+        elif (s1.count(letter) == s2.count(letter) and s1.count(letter) > 1):
+            result.append(["=", letter, s2.count(letter)])
+
+    result = sorted(result, key=lambda e: e[2], reverse=True)
+
+    result_s = "/".join([f"{element[0]}:{element[1] * element[2]}" for element in result])
+
+    return result_s
+
+print(mix("A generation must confront the looming ", "codewarrs"))
